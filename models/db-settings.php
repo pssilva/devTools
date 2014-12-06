b57f2650815f94b058125f608245373d47aa74d2
pssilva <pss1suporte@gmail.com>
2014-12-06 17:25:59 -0200

    Segundo commit

--- models/db-settings.php
+++ models/db-settings.php
@@ -6,10 +6,10 @@ http://usercake.com
 
 //Database Information
 $db_host = "localhost"; //Host address (most likely localhost)
-$db_name = "202"; //Name of Database
-$db_user = "202"; //Name of database user
-$db_pass = "password"; //Password for database user
-$db_table_prefix = "uc_";
+$db_name = "buscafacil_paulo"; //Name of Database
+$db_user = "buscafacil"; //Name of database user
+$db_pass = "c1o8g0Cw"; //Password for database user
+$db_table_prefix = "";
 
 GLOBAL $errors;
 GLOBAL $successes;
@@ -19,19 +19,23 @@ $successes = array();
 
 /* Create a new mysqli object with database connection parameters */
 $mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name);
+
+$mysqli->set_charset("utf8");
+$mysqli->query("SET NAMES 'utf8' COLLATE 'utf8_general_ci'");
+
 GLOBAL $mysqli;
 
 if(mysqli_connect_errno()) {
-	echo "Connection Failed: " . mysqli_connect_errno();
+	echo "Connection Failed: " . mysqli_connect_errno() ."<br />";
 	exit();
 }
 
 //Direct to install directory, if it exists
-if(is_dir("install/"))
-{
-	header("Location: install/");
-	die();
+// if(is_dir("install/"))
+// {
+// 	header("Location: install/");
+// 	die();
 
-}
+// }
 
 ?>
